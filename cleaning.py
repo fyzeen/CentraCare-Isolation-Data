d9aaaa6917a6fd08602ed757377e1dab43b8c379
fyzeen <afyzeen692@gmail.com>
2023-06-25 22:14:57 -0500

    completed further analyses

--- cleaning.py
+++ cleaning.py
@@ -43,6 +43,10 @@ if __name__ == '__main__':
 
     df = df.drop(["FLO_MEAS_ID", "Last Date", "DISP_NAME", "MEAS_VALUE", "MEAS_VALUE_EXTERNAL"], axis=1).drop_duplicates()
 
+    # Now we define a new row: Number of interactions (Item 2 in the SNI)
+    df['NumInteractions'] = df.apply(lambda row: row['GetTogether_Val'] + row['Phone_Val'] if row['GetTogether_Val'] != 98 and row['Phone_Val'] != 98 else (row['GetTogether_Val'] + row['Phone_Val'] - 196) if row['GetTogether_Val'] == 98 and row['Phone_Val'] == 98 else row['GetTogether_Val'] + row['Phone_Val'] - 98, axis=1)
+    df["NumInteractions"] = df["NumInteractions"] - 2 # Subtract two because GetTogether_Val==1 and Phone_Vla==1 representes 0 interactions via that medium
+
     # Define value to score mappings for each value
     Groups_YN_ValToScore = {1:1, 2:0, 98:0}
     Marriage_ValToScore = {3:1, 4:0, 5:0, 6:0, 7:0, 8:1, 98:0}
@@ -50,6 +54,7 @@ if __name__ == '__main__':
     GetTogether_ValToScore = {1:0, 2:0, 3:0, 4:1, 5:1, 98:0}
     Phone_ValToScore = {1:0, 2:0, 3:0, 4:1, 5:1, 98:0}
     NumGroupMeetings_ValToScore = {1:0, 2:0, 3:1, 98:0, None:0}
+    NumInteractions_ValToScore = {0:0,  7:1,  1:0,  4:1,  5:1,  3:1,  8:1,  6:1,  2:0, None:0, -2:0, -1:0}
 
     # Compute scores based on values
     df["Groups_YN_Score"] = df["Groups_YN_Val"].map(Groups_YN_ValToScore)
@@ -58,15 +63,13 @@ if __name__ == '__main__':
     df['GetTogether_Score'] = df['GetTogether_Val'].map(GetTogether_ValToScore)
     df['Phone_Score'] = df['Phone_Val'].map(Phone_ValToScore)
     df["NumGroupMeetings_Score"] = df["NumGroupMeetings_Val"].map(NumGroupMeetings_ValToScore)
+    df["NumInteractions_Score"] = df["NumInteractions"].map(NumInteractions_ValToScore)
 
     # Compute Risk Score (4+ = Socially Integrated; 3 = Moderately Integrated; 2 = Moderately Isolated; 1 or 0 = Socially Isolated)
-    df["SocialIntegrationScore"] = df["Groups_YN_Score"] + df["Marriage_Score"] + df['Religious_Score'] + df['GetTogether_Score']+ df['Phone_Score'] + df["NumGroupMeetings_Score"]
-    df["noMarriageSocialIntegrationScore"] = df["Groups_YN_Score"] + df['Religious_Score'] + df['GetTogether_Score']+ df['Phone_Score'] + df["NumGroupMeetings_Score"]
-
+    df["SocialIntegrationScore"] = df["Groups_YN_Score"] + df["Marriage_Score"] + df['Religious_Score'] + df["NumInteractions_Score"]
 
     # Use a cutoff of TWO (2) as the score under which a patient is classified as "isolated"
     df["Isolation_YN"] = (df["SocialIntegrationScore"] < 2).astype(int)
-    df["noMarriageIsolation_YN"] = (df["SocialIntegrationScore"] < 2).astype(int)
 
     # Keeping only patients >= 18 years and < 100 (because we have only (maximum of) 10 individuals per age > 100)
     df = df[df['Age'] >= 18]
